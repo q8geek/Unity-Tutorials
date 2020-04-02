b805d9071a2cd879ce6363d1a328dd081999e19e
q8geek <webmaster@q8geeks.org>
2020-04-02 22:13:31 +0300

    إضافة الدرس الأول

---
+++ First Tutorial/Assets/Scripts/MoveBullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rigidbody;
+    public float bulletSpeed = 50;
+
+    private void Start()
+    {
+        rigidbody.AddRelativeForce(Vector3.forward * bulletSpeed,ForceMode.VelocityChange);
+        Destroy(gameObject, 2f);
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Player")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ First Tutorial/Assets/Scripts/ShootBullets.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullets : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public GameObject shootFrom;
+    public float nextBullet = 0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (Time.timeSinceLevelLoad > nextBullet)
+            {
+                Instantiate(bulletPrefab, shootFrom.transform.position, shootFrom.transform.rotation);
+                nextBullet = Time.timeSinceLevelLoad + 1f;
+            }
+        }
+    }
+
+
+}
