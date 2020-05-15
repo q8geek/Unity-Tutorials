add1aaf11109b16bd74bf4a65e5a09def3b6b44a
q8geek <webmaster@q8geeks.org>
2020-05-16 02:15:49 +0300

    Adding sixth tutorial

---
+++ Sixth Tutorial/Assets/Scripts/BotBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotBehavior : MonoBehaviour
+{
+    float direction = 1f;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * direction);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "SwitchBot")
+        {
+            direction *= -1f;
+        }
+    }
+}
---
+++ Sixth Tutorial/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    Rigidbody2D rBody;
+    public float moveSpeed = 4f;
+    public float jumpPower = 10;
+    public bool jumping = false;
+    public bool canJump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime);
+        //if (Input.GetAxisRaw("Vertical") > 0f && canJump)
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && canJump)
+        {
+            rBody.velocity += Vector2.up * jumpPower;
+            canJump = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "CanJump")
+            canJump = true;
+    }
+}
